fac10c7396e614548bdd63412c6780ee58c499cf
bufanda <nhagen@iastate.edu>
2016-08-10 17:26:12 +0000

    added radar plot and accompanying logic

--- front-end/resultsDisplay.js
+++ front-end/resultsDisplay.js
@@ -16,6 +16,14 @@ function displayResults() {
     // gradient indicators for ecoscores
     drawEcosystemIndicatorsDisplay(currentYear);
 
+    var tempObj = [];
+    //test functions
+    for(var y = 1; y <= currentYear; y++){
+        tempObj.push(y) ;
+    }
+    drawEcosystemRadar(tempObj);
+    
+
     //toggle the arrows on the results page
     document.getElementById('resultsFrame').contentWindow.toggleYearForLandPlotBy(0);
     document.getElementById('resultsFrame').contentWindow.toggleYearForESIAsterBy(0);
@@ -1310,3 +1318,368 @@ function drawEcosystemIndicatorsDisplay(year) {
         });
 
 } //end drawEcosystemIndicatorsDisplay()
+
+
+//
+
+function drawEcosystemRadar(yearArray) {
+    
+    //clear info
+    document.getElementById('resultsFrame').contentWindow.document.getElementById('radarChart').innerHTML = " ";
+    document.getElementById('resultsFrame').contentWindow.document.getElementById('radarLegend').innerHTML = " ";
+    
+var w = 300,
+	h = 300;
+
+var colorscale = d3.scaleOrdinal(d3.schemeCategory10);
+var legendOptions = [] ;
+
+//Data
+var d = [] ;
+		  
+for(var i=0; i < yearArray.length; i++){
+    
+    var y = yearArray[i] ;
+    
+    var obj = [ 
+            {label:"Nitrate Concentration", axis:"Nitrate", value: Totals.nitrateConcentrationScore[y] / 100, raw: (Math.round(Totals.nitrateConcentration[y]*10) / 10) + " ppm" },
+			{label:"Phosphorus Load", axis:"Phosphorus", value: Totals.phosphorusLoadScore[y] / 100, raw: (Math.round(Totals.phosphorusLoad[y]*10) / 10) + " tons" },
+			{label:"Sediment Delivery",axis: "Sediment", value: Totals.sedimentDeliveryScore[y] / 100, raw: (Math.round(Totals.sedimentDelivery[y]*10) / 10) + " tons"},
+			{label:"Carbon Sequestration", axis: "Carbon", value: Totals.carbonSequestrationScore[y] / 100, raw: (Math.round(Totals.carbonSequestration[y]*10) / 10) + " tons"},
+			{label:"Gross Erosion", axis: "Erosion", value: Totals.grossErosionScore[y] / 100, raw: (Math.round(Totals.grossErosion[y]*10) / 10) + " tons"},
+			{label:"Game Wildlife", axis: "Wildlife", value: Totals.gameWildlifePointsScore[y] / 100, raw: (Math.round(Totals.gameWildlifePoints[y]*10) / 10) + " pts"},
+			{label:"Biodiversity", axis: "Biodiversity", value: Totals.biodiversityPointsScore[y] / 100, raw: (Math.round(Totals.biodiversityPoints[y]*10) / 10) + " pts"} 
+			];
+			
+    d.push(obj) ;
+    legendOptions.push("Year " + y ) ;
+}		  
+			
+		  	
+
+//Options for the Radar chart, other than default
+var mycfg = {
+  w: w,
+  h: h,
+  maxValue: 1,
+  levels: 5,
+  ExtraWidthX: 300
+}
+
+var radarId = document.getElementById('resultsFrame').contentWindow.document.getElementById('radarChart');
+var radarLegendId = document.getElementById('resultsFrame').contentWindow.document.getElementById('radarLegend');
+
+//Call function to draw the Radar chart
+//Will expect that data is in %'s
+RadarChart.draw(radarId, d, mycfg);
+    
+    
+var width = 355 ;
+var height = 370;
+
+//draw legend and title
+var svg = d3.select(radarLegendId)
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+        
+        
+     svg.append("text")
+        .attr("x", 0)
+        .attr("y", -120)
+        .attr("text-anchor", "middle")
+        .style("font-size", "25px")
+        .style("font-weight", "bold")
+        .text("Ecosystem Services");        
+    
+//sizing for the colored squares and spaces 
+    var legendRectSize = 18;
+    var legendSpacing = 4;
+
+    //add all the elements that have a nonzero count
+    var legend = svg.selectAll('.legend')
+        .data(legendOptions)
+        .enter()
+        .append('g')
+        .on('mouseover', function(d) {
+           
+            d3.select(this).style("fill", "steelblue");
+            
+            var g = d3.select(radarId) ;
+            z = d3.select(this).attr("childElement") ;
+            z = "polygon." + z;
+            
+            		g.selectAll("polygon")
+						.transition(200)
+						.style("fill-opacity", 0.1); 
+					g.selectAll(z)
+						.transition(200)
+						.style("fill-opacity", .7);
+						
+            
+        })
+        .on('mouseout', function(d) {
+            
+            d3.select(this).style("fill", "black");
+            
+            var g = d3.select(radarId) ;
+            g.selectAll("polygon")
+				.transition(200)
+				.style("fill-opacity", 0.2);
+            
+        })
+        .attr("childElement", function(d,i){
+            return "radar-chart-serie" + i ;
+        })
+        .attr('transform', function(d, i) {
+            var height = legendRectSize + legendSpacing;
+            var offset = 80 ;
+            var horz = -117;
+            var vert = i * height - offset;
+            return 'translate(' + horz + ',' + vert + ')';
+        });
+
+    //add legend color squares
+    legend.append('rect')
+        .attr('width', legendRectSize)
+        .attr('height', legendRectSize)
+        .style('fill', function(d, i) {
+            return colorscale(i) ;
+        })
+        .style('stroke', function(d, i) {
+            return colorscale(i) ;
+        });
+
+    //add legend text info 
+    legend.append('text')
+        .attr('x', legendRectSize + legendSpacing)
+        .attr('y', legendRectSize - legendSpacing)
+        .text(function(d) {
+            return d;
+        });    
+
+    
+    
+}
+
+
+
+////credits
+//http://bl.ocks.org/nbremer/6506614
+
+var RadarChart = {
+  draw: function(id, d, options){
+  var cfg = {
+	 radius: 5,
+	 w: 600,
+	 h: 600,
+	 factor: 1,
+	 factorLegend: .85,
+	 levels: 3,
+	 maxValue: 0,
+	 radians: 2 * Math.PI,
+	 opacityArea: 0.2,
+	 ToRight: 5,
+	 TranslateX: 70,
+	 TranslateY: 30,
+	 ExtraWidthX: 100,
+	 ExtraWidthY: 100,
+	 color: d3.scaleOrdinal(d3.schemeCategory10)
+	};
+	
+	if('undefined' !== typeof options){
+	  for(var i in options){
+		if('undefined' !== typeof options[i]){
+		  cfg[i] = options[i];
+		}
+	  }
+	}
+	cfg.maxValue = Math.max(cfg.maxValue, d3.max(d, function(i){return d3.max(i.map(function(o){return o.value;}))}));
+	var allAxis = (d[0].map(function(i, j){return i.axis}));
+	var total = allAxis.length;
+	var radius = cfg.factor*Math.min(cfg.w/2, cfg.h/2);
+	var Format = d3.format('%');
+	d3.select(id).select("svg").remove();
+	
+	var g = d3.select(id)
+			.append("svg")
+			.attr("width", cfg.w+250 )
+			.attr("height", cfg.h+cfg.ExtraWidthY)
+			.append("g")
+			.attr("transform", "translate(" + cfg.TranslateX + "," + cfg.TranslateY + ")");
+			;
+
+  var mouseoverInfo = d3.select(id)
+        .append('g')
+        .attr('class', 'mouseoverInfoRadar');
+
+    mouseoverInfo.append('div')
+        .attr('class', 'label');
+
+    mouseoverInfo.append('div')
+        .attr('class', 'count');
+
+    mouseoverInfo.append('div')
+        .attr('class', 'score');
+
+	//Circular segments
+	for(var j=0; j<cfg.levels-1; j++){
+	  var levelFactor = cfg.factor*radius*((j+1)/cfg.levels);
+	  g.selectAll(".levels")
+	   .data(allAxis)
+	   .enter()
+	   .append("svg:line")
+	   .attr("x1", function(d, i){return levelFactor*(1-cfg.factor*Math.sin(i*cfg.radians/total));})
+	   .attr("y1", function(d, i){return levelFactor*(1-cfg.factor*Math.cos(i*cfg.radians/total));})
+	   .attr("x2", function(d, i){return levelFactor*(1-cfg.factor*Math.sin((i+1)*cfg.radians/total));})
+	   .attr("y2", function(d, i){return levelFactor*(1-cfg.factor*Math.cos((i+1)*cfg.radians/total));})
+	   .attr("class", "line")
+	   .style("stroke", "grey")
+	   .style("stroke-opacity", "0.75")
+	   .style("stroke-width", "0.3px")
+	   .attr("transform", "translate(" + (cfg.w/2-levelFactor) + ", " + (cfg.h/2-levelFactor) + ")");
+	}
+
+	//Text indicating at what % each level is
+	for(var j=0; j<cfg.levels; j++){
+	  var levelFactor = cfg.factor*radius*((j+1)/cfg.levels);
+	  g.selectAll(".levels")
+	   .data([1]) //dummy data
+	   .enter()
+	   .append("svg:text")
+	   .attr("x", function(d){return levelFactor*(1-cfg.factor*Math.sin(0));})
+	   .attr("y", function(d){return levelFactor*(1-cfg.factor*Math.cos(0));})
+	   .attr("class", "legend")
+	   .style("font-family", "sans-serif")
+	   .style("font-size", "10px")
+	   .attr("transform", "translate(" + (cfg.w/2-levelFactor + cfg.ToRight) + ", " + (cfg.h/2-levelFactor) + ")")
+	   .attr("fill", "#737373")
+	   .text( ((j+1)*cfg.maxValue/cfg.levels).toFixed(2) * 100  );
+	}
+	
+	series = 0;
+
+	var axis = g.selectAll(".axis")
+			.data(allAxis)
+			.enter()
+			.append("g")
+			.attr("class", "axis");
+
+	axis.append("line")
+		.attr("x1", cfg.w/2)
+		.attr("y1", cfg.h/2)
+		.attr("x2", function(d, i){return cfg.w/2*(1-cfg.factor*Math.sin(i*cfg.radians/total));})
+		.attr("y2", function(d, i){return cfg.h/2*(1-cfg.factor*Math.cos(i*cfg.radians/total));})
+		.attr("class", "line")
+		.style("stroke", "grey")
+		.style("stroke-width", "1px");
+
+	axis.append("text")
+		.attr("class", "legend")
+		.text(function(d){return d})
+		.style("font-family", "sans-serif")
+		.style("font-size", "16px")
+		.attr("text-anchor", "middle")
+		.attr("dy", "1.5em")
+		.attr("transform", function(d, i){return "translate(0, -17)"})
+		.attr("x", function(d, i){return cfg.w/2*(1-cfg.factorLegend*Math.sin(i*cfg.radians/total))-60*Math.sin(i*cfg.radians/total);})
+		.attr("y", function(d, i){return cfg.h/2*(1-Math.cos(i*cfg.radians/total))-20*Math.cos(i*cfg.radians/total);});
+
+ 
+	d.forEach(function(y, x){
+	  dataValues = [];
+	  g.selectAll(".nodes")
+		.data(y, function(j, i){
+		  dataValues.push([
+			cfg.w/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total)), 
+			cfg.h/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total))
+		  ]);
+		});
+	  dataValues.push(dataValues[0]);
+	  g.selectAll(".area")
+					 .data([dataValues])
+					 .enter()
+					 .append("polygon")
+					 .attr("class", "radar-chart-serie"+series)
+					 .style("stroke-width", "2px")
+					 .style("stroke", cfg.color(series))
+					 .attr("points",function(d) {
+						 var str="";
+						 for(var pti=0;pti<d.length;pti++){
+							 str=str+d[pti][0]+","+d[pti][1]+" ";
+						 }
+						 return str;
+					  })
+					 .style("fill", function(j, i){return cfg.color(series)})
+					 .style("fill-opacity", cfg.opacityArea)
+					 .on('mouseover', function (d){	 
+					    
+					 })
+					 .on('mouseout', function(){ 
+					    					 });
+	  series++;
+	});
+	series=0;
+
+
+	d.forEach(function(y, x){
+	  g.selectAll(".nodes")
+		.data(y).enter()
+		.append("svg:circle")
+		.attr("class", "radar-chart-serie"+series)
+		.attr('r', cfg.radius)
+		.attr("alt", function(j){return Math.max(j.value, 0)})
+		.attr("cx", function(j, i){
+		  dataValues.push([
+			cfg.w/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total)), 
+			cfg.h/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total))
+		]);
+		return cfg.w/2*(1-(Math.max(j.value, 0)/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total));
+		})
+		.attr("cy", function(j, i){
+		  return cfg.h/2*(1-(Math.max(j.value, 0)/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total));
+		})
+		.attr("data-id", function(j){return j.axis})
+		.style("fill", function(d, i) { 
+		    return cfg.color(series) ;
+		})
+		.style("fill-opacity", .9)
+		.on('mouseover', function (d){
+					newX =  parseFloat(d3.select(this).attr('cx')) - 10;
+					newY =  parseFloat(d3.select(this).attr('cy')) - 5;
+					
+					z = "polygon."+d3.select(this).attr("class");
+					g.selectAll("polygon")
+						.transition(200)
+						.style("fill-opacity", 0.1); 
+					g.selectAll(z)
+						.transition(200)
+						.style("fill-opacity", .7);
+					
+					d3.select(this).attr("r", 15) ;
+					
+				    mouseoverInfo.select('.label').html(d.label);
+                    mouseoverInfo.select('.count').html(d.raw);
+                    mouseoverInfo.select('.score').html(Math.round(d.value * 1000) / 10 + "/100");
+                    mouseoverInfo.style('border-color', d3.select(this).style("fill"));
+                    mouseoverInfo.style('display', 'block');
+						
+				 })
+		.on('mouseout', function(){
+		            d3.select(this).attr("r", cfg.radius);
+					mouseoverInfo.style('display', 'none');
+					
+					g.selectAll("polygon")
+						.transition(200)
+						.style("fill-opacity", cfg.opacityArea);
+					
+				 })
+		.text(function(j){return Math.max(j.value, 0)});
+
+	  series++;
+	});
+	
+  }
+};
