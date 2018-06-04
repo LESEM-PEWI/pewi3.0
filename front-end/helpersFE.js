ee2ca9fe92fbdbbbcc549a99e677e3150ad7dc2b
WeijiaZhao <weijia.h.zhao@gmail.com>
2018-06-04 15:59:08 -0500

    Resolve again

--- front-end/helpersFE.js
+++ front-end/helpersFE.js
@@ -2275,7 +2275,7 @@ function multiplayerMode() {
     document.getElementById("playerAddButton").style.display = "inline-block";
     document.getElementById("playerResetButton").style.display = "block";
     document.getElementById("levelsButton").style.display = "none";
-    document.getElementById("yearButton").style.display = "none";
+    // document.getElementById("yearButton").style.display = "none";
     document.getElementById("yearButton").style.display = "block";
     // When hit download button, it should download the multi-map.
     document.getElementById("DownloadButton").onclick = endMultiplayerAssignMode;
