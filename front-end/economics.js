ea2a30d0b8c977cc9ed8110f047889b1b79cc50f
Jian Kai Lee <lee_jk1996@hotmail.com>
2019-06-24 14:03:54 -0500

    grahpic 4 data

--- front-end/economics.js
+++ front-end/economics.js
@@ -2,14 +2,17 @@ var economics = (function () { //Singleton for getting economics data from the b
   var instance;
   var rawData;
   var data;
-  function init() {
+  var data2=[];
 
+  function init() {
+    console.log(this);
     function divideByCategory (listofCats){
       console.log(this.rawData);
+      console.log(this.data);
       listofCats.forEach(cat => {
         this.rawData.forEach(dataPoint => {
           if (!this.data[dataPoint['LU_ID']]) {
-            this.data[dataPoint['LU_ID']] = {'landUse': dataPoint['Land-Use']}
+            this.data[dataPoint['LU_ID']] = {'landUse': dataPoint['Land-Use']};
           } // We need to create a path to the data that we want to pull out
           if (!this.data[dataPoint['LU_ID']][cat]){ //if this is the first time that we see a particular category for a particular land use
             this.data[dataPoint['LU_ID']][cat] = {total: 0}; //Further path making
@@ -29,18 +32,35 @@ var economics = (function () { //Singleton for getting economics data from the b
       });
       console.log(this.data)
     }
+    function chart4Information(lists) {
+      console.log(data2);
+      //lists.forEach(cat => {
+        this.rawData.forEach(dataPoint => {
+          if (!data2[dataPoint['LU_ID']]) {
+            data2[dataPoint['LU_ID']] = {'landUse': dataPoint['Land-Use'],'array':[]}
+          } // We need to create a path to the data that we want to pull out
+          console.log(dataPoint);
+          data2[dataPoint['LU_ID']]['array'].push(dataPoint);
+        });
+      //});
+      console.log(data2);
+    }
+
     d3.csv('./budgets.csv', function(data){
       this.rawData = data;
-      divideByCategory(['Action - Cost Type', 'Time - Cost Type', 'Fixed/Variable'])
+      divideByCategory(['Action - Cost Type', 'Time - Cost Type', 'Fixed/Variable']);
+      chart4Information(['Action - Cost Type', 'Time - Cost Type', 'Fixed/Variable']);
     })
     return {//public fields
       data: this.data,
-      rawData: this.rawData
+      rawData: this.rawData,
+      data2: this.data2
     };
   };
   return {
     getInstance: function () { //To ensure singularity
       if ( !instance ) {
+        console.log(this.data);
         instance = init();
       }
       return instance;
